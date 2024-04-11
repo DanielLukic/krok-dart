f178c867d3355e23207809d8fb7be71e54cf4615
Daniel Lukic <daniel.lukic@gmx.net>
2024-04-11 17:19:05 +0200

    Add balance -x flag to retrieve extended balanace

--- lib/src/cli/command/balance.dart
+++ lib/src/cli/command/balance.dart
@@ -19,15 +19,39 @@ class Balance extends Command with ApiCall, Tabular {
       defaultsTo: false,
       callback: (it) => small = it,
     );
+    argParser.addFlag(
+      "extended",
+      abbr: "x",
+      help: "Retrieve extended balances.",
+      defaultsTo: false,
+      callback: (it) => extended = it,
+    );
   }
 
   bool small = false;
+  bool extended = false;
 
   @override
   autoClose(KrakenApi api) async {
-    final Result result = await api.retrieve(KrakenRequest.balance());
-    final filtered = result.where((p0, p1) => small || double.parse(p1) >= 0.000001);
-    final data = filtered.asVerticalTableData();
-    processResultList(data, ["pair", "volume"]);
+    final request =
+        extended ? KrakenRequest.balanceEx() : KrakenRequest.balance();
+    final Result result = await api.retrieve(request);
+    if (extended) {
+      final filtered = result
+          .where((p0, p1) => small || double.parse(p1['balance']) > 0.00001);
+
+      final rows = filtered.entries
+          .map((e) => [e.key, e.value['balance'], e.value['hold_trade']]
+              .map((e) => e.toString())
+              .toList())
+          .toList();
+
+      processResultList(rows, ['asset', 'balance', 'hold_trade']);
+    } else {
+      final filtered =
+          result.where((p0, p1) => small || double.parse(p1) > 0.00001);
+      final data = filtered.asVerticalTableData();
+      processResultList(data, ["pair", "volume"]);
+    }
   }
 }
